2de50a2ac4429aac762ed0f1827089091baa4374
hnguyen17 <hoangng1710@gmail.com>
2023-12-30 16:58:55 +0700

    Handle the case that the dir is not exist.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -4,8 +4,14 @@ writefile=$1
 writestr=$2
 
 if [ $# -eq 2 ];then
-    echo $writestr > $writefile || { echo "File could not be created"; exit 1; }
-    exit 0
+    filedir=${writefile%/*}
+    if [ -d $filedir ]; then
+        echo $writestr > $writefile || { echo "File could not be created";exit 1; }
+        exit 0
+    else
+        mkdir -p "$filedir" && echo $writestr > $writefile 
+        exit 0
+    fi
 else
     exit 1
 fi
