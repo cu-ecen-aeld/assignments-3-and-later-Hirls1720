0ab2ad0aa26e8b93e4a31b8923722dc4cdba0743
hnguyen17 <hoangng1710@gmail.com>
2023-12-30 17:05:03 +0700

    Missunderstand finder.sh requirement for number of files and subdirectories
    I thought the requirement is count all the files under the directory
    including those files under of its subdirectories

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -5,7 +5,7 @@ searchstr=$2
 
 if [ $# -eq 2 ] ;then
     if [ -d $filesdir ]; then
-        x="$(find . -type f | wc -l)"
+        x="$(ls $filesdir  | wc -l)"
         y=$(grep -r $searchstr $filesdir | wc -l )
         echo "The number of files are $x and the number of matching lines are $y"
         exit 0
